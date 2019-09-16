8589e13c8aa92a33600bdd33c023b3801a8ffb29
Eliane Quadro <46668864+elianequadro@users.noreply.github.com>
2019-09-16 13:02:14 -0300

    react tutorial
    https://www.taniarascia.com/getting-started-with-react/

---
+++ precache-manifest.de5bcdad90e6fc4b984b6f4a22303051.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed35a94fdab91bc6a3ddd8f48bb971c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a9eee016331bdeb0f67",
+    "url": "/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "854d68ec4190dad53f1b",
+    "url": "/static/js/2.d7b9399b.chunk.js"
+  },
+  {
+    "revision": "3a9eee016331bdeb0f67",
+    "url": "/static/js/main.5da948cd.chunk.js"
+  },
+  {
+    "revision": "53ba363798496421b86f",
+    "url": "/static/js/runtime~main.5098043e.js"
+  }
+]);
